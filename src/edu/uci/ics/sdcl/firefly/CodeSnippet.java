0cf9a0bcacf2f70c0a689c859e3b16917a1548db
Danilo Cardoso <cardoso.nilo@gmailcom>
2014-06-05 16:14:22 -0700

    CodeSnippet class

--- src/edu/uci/ics/sdcl/firefly/CodeSnippet.java
+++ src/edu/uci/ics/sdcl/firefly/CodeSnippet.java
@@ -1,16 +1,35 @@
+// package edu.uci.ics.sdcl.firefly;
+import java.util.ArrayList;
+
 public class CodeSnippet
 {
-	private String uniqueName, visibilityType, implementationType, methodBody;
-	private String methodInterface, returnStatment, methodLineNumber;
+	protected String uniqueName; 			// path + name of method
+	protected String visibilityType;		// public, protected or private
+	protected String implementationType; 	// concrete or abstract
+	protected String methodBody;			// whole content of method
+	protected String methodInterface;		// method declaration
+	protected Boolean returnStatment;		// true if there is a return value
+	protected MethodDeclaration methodDeclaration;	// parsed method declaration
+	protected ArrayList<CodeElement> statments;		// flow control statments
 	
-	public CodeSnippet()
+	
+	public CodeSnippet(String uniqueName, String visibilityType, String implementationType, 
+			String methodBody, String methodInterface, Boolean returnStatment, 
+			MethodDeclaration methodDeclaration, ArrayList<CodeElement> statments)
 	{
-		
+		this.uniqueName = uniqueName;
+		this.visibilityType = visibilityType;
+		this.implementationType = implementationType;
+		this.methodBody = methodBody;
+		this.methodInterface = methodInterface;
+		this.returnStatment = returnStatment;
+		this.methodDeclaration = methodDeclaration;
+		this.statments = statments;
 	}
 	
 	public void print()
 	{
-		
+		//TO DO
 	}
 	
 }
