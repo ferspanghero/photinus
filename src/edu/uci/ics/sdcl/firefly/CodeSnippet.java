4e07cb6c519019e5c2fbf979cf5fb32d048313ec
Danilo Cardoso <cardoso.nilo@gmailcom>
2014-06-05 14:51:59 -0700

    Adding again and again

---
+++ src/edu/uci/ics/sdcl/firefly/CodeSnippet.java
@@ -0,0 +1,16 @@
+public class CodeSnippet
+{
+	private String uniqueName, visibilityType, implementationType, methodBody;
+	private String methodInterface, returnStatment, methodLineNumber;
+	
+	public CodeSnippet()
+	{
+		
+	}
+	
+	public void print()
+	{
+		
+	}
+	
+}
