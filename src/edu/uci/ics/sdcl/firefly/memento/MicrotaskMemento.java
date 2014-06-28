b59004776648fcb514175be58ebfb14feb963137
Christian Medeiros Adriano <christian.adriano@gmail.com>
2014-06-27 17:11:40 -0700

    Testing Persistence of Microtasks

--- src/edu/uci/ics/sdcl/firefly/memento/MicrotaskMemento.java
+++ src/edu/uci/ics/sdcl/firefly/memento/MicrotaskMemento.java
@@ -2,6 +2,7 @@ package edu.uci.ics.sdcl.firefly.memento;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -18,42 +19,114 @@ import edu.uci.ics.sdcl.firefly.Microtask;
  *
  */
 public class MicrotaskMemento {
+
+	/** Holds the list of microtasks for all files. 
+	 * String key = file name, ArrayList is the list of microtasks*/
+	private HashMap<String,ArrayList<Microtask>> debugSessionMicrotaskMap;
+
+	private String persistentFileName = "c:/Users/Christian Adriano/My Documents/microtasks.ser";
 	
-	private ArrayList<HashMap<String, Microtask>> debugSessionMicrotasks;
-	
-	private String persistentFileName = "microtasks.ser";
-	
+	private File file;
+
 	public MicrotaskMemento(){
-	
-		try{
+
+		/*try{
 			
-		//Try to retrieve an existing file.
-		ObjectInputStream objInputStream = new ObjectInputStream( 
-                 // By using "FileOutputStream" we will 
-                 // Read it to a File in the file system
-                 new FileInputStream(new File(this.persistentFileName)));
+			//Check if the files exists
+			file = new File(this.persistentFileName);
+			
+		}
+		catch(FileNotFoundException exception){
+			
+		}*/
 		
-		debugSessionMicrotasks = (ArrayList<HashMap<String, Microtask>>) objInputStream.readObject();
+		try{
+			file = new File(this.persistentFileName);
+			if(file.exists() && !file.isDirectory()){
+				
+			//Try to retrieve an existing file.
+			ObjectInputStream objInputStream = new ObjectInputStream( 
+					// By using "FileOutputStream" we will 
+					// Read it from a File in the file system
+					new FileInputStream(file));
+
+			this.debugSessionMicrotaskMap = (HashMap<String,ArrayList<Microtask>>) objInputStream.readObject();
+
+			 ((ObjectOutput) objInputStream).writeObject( debugSessionMicrotaskMap );
+				objInputStream.close();
+			}
+			// No files has been created yet. 
+			else{
+				// Create a sample object, that contains the default values.
+				this.debugSessionMicrotaskMap = new HashMap<String,ArrayList<Microtask>>();
+
+				ObjectOutputStream objOutputStream = new ObjectOutputStream( 
+						// By using "FileOutputStream" we will 
+						// Write it to a File in the file system
+						new FileOutputStream(new File(this.persistentFileName)));
+
+				objOutputStream.writeObject( debugSessionMicrotaskMap );
+				objOutputStream.close();
+			}
+
+		}
+		catch(IOException exception){
+			 exception.printStackTrace();
+		}
+		catch(Exception exception){
+			 exception.printStackTrace();
+		}
+	}
+
+	/** Retrieves the list of microtasks for provided file
+	 * 
+	 * @param fileName
+	 * @return
+	 */
+	public ArrayList<Microtask> read(String fileName){
 		
-		if(this.debugSessionMicrotasks!=null){
-			((ObjectOutput) objInputStream).writeObject( debugSessionMicrotasks );
+		try{	
+			ObjectInputStream objInputStream = new ObjectInputStream( 
+					// By using "FileOutputStream" we will 
+					// Read it to a File in the file system
+					new FileInputStream(new File(this.persistentFileName)));
+
+			this.debugSessionMicrotaskMap = (HashMap<String,ArrayList<Microtask>>) objInputStream.readObject();
 			objInputStream.close();
-		
+			
+			return this.debugSessionMicrotaskMap.get(fileName);
 		}
-		// No files has been created yet. 
-        else{
-			// Create a sample object, that contains the default values.
-			this.debugSessionMicrotasks = new ArrayList<HashMap<String, Microtask>>();
-
-	        ObjectOutputStream objOutputStream = new ObjectOutputStream( 
-	                               // By using "FileOutputStream" we will 
-	                               // Write it to a File in the file system
-	                                new FileOutputStream(new File(this.persistentFileName)));
-
-	        objOutputStream.writeObject( debugSessionMicrotasks );
-	        objOutputStream.close();
+		catch(IOException exception){
+			System.err.print("Error while opening microtasks serialized file:" + exception.toString());
+			return null;
 		}
-		
+		catch(Exception exception){
+			System.err.print("Error while opening microtasks serialized file:" + exception.toString());
+			return null;
+		}
+	}
+
+	public void insert(String key, Microtask microtask){
+		//TO DO
+	}
+
+	/** Insert a new List of Microtasks. It overwrites any existing one for the same file.
+	 * 
+	 * @param fileName
+	 * @param microtaskList
+	 */
+	public void insert(String fileName, ArrayList <Microtask> microtaskList){
+		try{
+
+			this.debugSessionMicrotaskMap.put(fileName, microtaskList);
+
+			ObjectOutputStream objOutputStream = new ObjectOutputStream( 
+					// By using "FileOutputStream" we will 
+					// Write it to a File in the file system
+					new FileOutputStream(new File(this.persistentFileName)));
+
+			objOutputStream.writeObject( debugSessionMicrotaskMap );
+			objOutputStream.close();
 		}
 		catch(IOException exception){
 			System.err.print("Error while opening microtasks serialized file:" + exception.toString());
@@ -62,24 +135,11 @@ public class MicrotaskMemento {
 			System.err.print("Error while opening microtasks serialized file:" + exception.toString());
 		}
 	}
-	
-	public Microtask read(String key){
-		//TO DO
-		return null;
-	}
-	
-	public void insert(String key, Microtask microtask){
-		//TO DO
-	}
-	
-	public void insert(ArrayList <Microtask> microtaskList){
-		//TO DO
-	}
-	
+
 	public void update(String key, Microtask microtask){
 		//TO DO
 	}
-	
+
 	public void delete(String key){
 		//TO DO
 	}
---
+++ src/test/MicrotaskMementoTest.java
@@ -0,0 +1,74 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.uci.ics.sdcl.firefly.CodeSnippet;
+import edu.uci.ics.sdcl.firefly.MethodParameter;
+import edu.uci.ics.sdcl.firefly.MethodSignature;
+import edu.uci.ics.sdcl.firefly.Microtask;
+import edu.uci.ics.sdcl.firefly.Question;
+import edu.uci.ics.sdcl.firefly.memento.MicrotaskMemento;
+
+public class MicrotaskMementoTest {
+
+	private  HashMap<String,ArrayList<Microtask>> debugSessionMicrotaskMap = new HashMap<String, ArrayList<Microtask>>();
+	private ArrayList<Microtask> microtaskList;
+	
+	@Before
+	public void setUp() throws Exception {
+		Question question = new Question();
+		MethodSignature signature = new MethodSignature("factorial", "public", new Integer(12));
+		MethodParameter arg1, arg2;
+		arg1 = new MethodParameter("Integer", "Seed");
+		arg2 = new MethodParameter("Integer", "Iterations");
+		signature.addMethodParameters(arg1);
+		signature.addMethodParameters(arg2);
+			
+		StringBuffer buffer = new StringBuffer("public Integer factorial(Integer Seed, Integer Iterations){");
+		buffer.append("\n");
+		buffer.append("if(Seed!=null){");
+		buffer.append("\n");
+		buffer.append("int aux=1;");
+		buffer.append("\n");
+		buffer.append("for (int i=0;i<Iterations.intValue();i++){");
+		buffer.append("\n");
+		buffer.append("aux =  aux * Seed;");
+		buffer.append("\n");
+		buffer.append("	}");
+		buffer.append("\n");
+		buffer.append("	return new Integer(aux);");
+		buffer.append("\n");
+		buffer.append("}");
+		buffer.append("\n");
+		buffer.append("else return null;");
+		buffer.append("\n");
+		buffer.append("}");
+		
+		CodeSnippet codeSnippetFactorial=new CodeSnippet("sample","SimpleSampleCode", buffer, 
+				new Boolean (true), signature);
+		Microtask mtask = new Microtask(question, codeSnippetFactorial);
+		
+		//Create the data structure
+		this.microtaskList =  new ArrayList<Microtask>();
+		microtaskList.add(mtask);
+		
+		this.debugSessionMicrotaskMap.put("SimpleSampleCode.java", microtaskList);
+	}
+
+	@Test
+	public void testCreateNewPersistentFile() {
+		
+		MicrotaskMemento memento = new MicrotaskMemento();
+		memento.insert("SimpleSampleCode.java", this.debugSessionMicrotaskMap.get("SimpleSampleCode.java"));
+		
+		ArrayList<Microtask>mlist = memento.read("SimpleSampleCode.java");
+		assertSame(this.microtaskList,mlist);
+	}
+
+}
