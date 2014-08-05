be051a48ca99437533c9357daee45fecf3a9f1d8
Christian Medeiros Adriano <christian.adriano@gmail.com>
2014-08-05 16:36:04 -0700

    Pre-task tests
    Code to test people's java skill.

---
+++ src/pretask/test/PositionFinder.java
@@ -0,0 +1,23 @@
+package pretask.test;
+public class PositionFinder {
+
+	public static void findPosition(String[] NameArray, String term){
+
+		if (NameArray.length==0)
+			System.out.print("Empty array");
+		else{ 
+			int position=-1;
+			for (String name: NameArray){
+				if(name.compareTo(term)==0)
+					break;		
+				position++;
+			}
+			System.out.print(position > 0? position: 0);
+		}
+	}
+
+	public static void main(String[] args){
+		String[] NameArray = {"Hola", "Kumusta", "Hello", "Ciao"};
+		findPosition(NameArray, " ");
+	}
+}
