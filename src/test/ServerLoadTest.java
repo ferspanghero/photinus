011dcf5b4546adbb1fa454149aeffbc65368263f
Christian Medeiros Adriano <christian.adriano@gmail.com>
2014-10-14 20:47:07 -0700

    Test: Implementing load test
    JWebUnit partially working

--- src/test/ServerLoadTest.java
+++ src/test/ServerLoadTest.java
@@ -1,7 +1,8 @@
 package test;
 
-import org.junit.Assert.*;
-import net.sourceforge.jwebunit.junit.JWebUnit.*;
+import static org.junit.Assert.*;
+import static net.sourceforge.jwebunit.junit.JWebUnit.*;
+import net.sourceforge.jwebunit.junit.JWebUnit;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -15,17 +16,23 @@ public class ServerLoadTest {
 
 	@Before
 	public void prepare() {
-		setBaseUrl("http://localhost:8080/test");
+		setBaseUrl("http://localhost:8080/crowd-debug-firefly/");
 	}
 
 	@Test
-	public void testLogin() {
-		beginAt("/home");
-		clickLink("login");
-		assertTitleEquals("Login");
-		setTextField("username", "test");
+	public void testConsentForm() {
+		//Consent Page
+		beginAt("ConsentForm.jsp");
+		JWebUnit.setLabeledFormElementField("consentBox", "1");
+		clickButton("Yes, I want to participate");
+		
+		//Test Page
+		assertTitleEquals("Java Skill Test");
+
+		/*setTextField("username", "test");
 		setTextField("password", "test123");
 		submit();
 		assertTitleEquals("Welcome, test!");
+		*/
 	}
 }
