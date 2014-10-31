ac79eb2899f243acdc053d7dd6ca086275de382f
Christian Medeiros Adriano <adrianoc@uci.edu>
2014-10-30 18:27:58 -0700

    Data Analysis: New method for

--- src/edu/uci/ics/sdcl/firefly/report/LogAnalysis.java
+++ src/edu/uci/ics/sdcl/firefly/report/LogAnalysis.java
@@ -65,14 +65,14 @@ public class LogAnalysis {
 		//System.out.println(" *** BUG REVEALING QUESTIONS - EXPECTED YES/Prob YES ANSWERS: ");
 		counterAnswers = counterYes+counterNos+counterICantTell;
 		System.out.println(fileName+ " Bug Revealing Answers="+counterAnswers);
-		System.out.print("#Yes="+counterYes+ " = "+ formatter.format(counterYes/(counterNos+counterYes)*100)+ "%, ");
-		System.out.print("#Nos="+counterNos + " = "+ formatter.format(counterNos/(counterYes+counterNos)*100)+ "%, ");
-		System.out.println("#ICantTell="+counterICantTell + " = "+ formatter.format(counterICantTell/(counterAnswers)*100)+ "%");
+		System.out.println("counterYes="+counterYes+ " = "+ formatter.format(counterYes/(counterYes+counterNos)*100)+ "%");
+		System.out.println("counterNos="+counterNos + " = "+ formatter.format(counterNos/(counterYes+counterNos)*100)+ "%");
+		System.out.println("counterICantTell="+counterICantTell + " = "+ formatter.format(counterICantTell/(counterAnswers)*100)+ "%");
 	
 		this.counterMap.put(fileName,new Counter(counterYes,counterNos, counterICantTell, fileName,counterAnswers));
 
 		
-		//System.out.println("-------------------------------------------");
+		System.out.println("-------------------------------------------");
 		
 	}
 
@@ -116,6 +116,60 @@ public class LogAnalysis {
 
 	}
 	
+
+	/**
+	 * Removed Method Body questions from the set.
+	 */
+	public void answersPerMethod(boolean include,HashMap<String,String> methodMap, HashMap<String, Microtask>taskMap){
+		System.out.println(" *** ANSWERS PER MICROTASK QUESTION: ");
+		double yes=0;
+		double total=0;
+		double no=0;
+		double iCantTell=0;
+		
+		Iterator<String> iter = taskMap.keySet().iterator();
+		while(iter.hasNext()){
+			String key = iter.next();
+			Microtask task = taskMap.get(key);
+			
+			//takes the situation for Yes and Nos questions
+			if(include && methodMap.containsKey(task.getID().toString()) ||
+					(!include && !methodMap.containsKey(task.getID().toString())))
+			{
+				yes=0;
+				no=0;
+				total=0;
+				iCantTell=0;
+				System.out.print(task.getID()+"|"+ task.getQuestion()+"|");
+				Vector<Answer> answerList = task.getAnswerList();
+				for(Answer answer: answerList){
+					
+					if(answer.getOption().trim().matches(Answer.YES) || answer.getOption().trim().matches(Answer.PROBABLY_YES))
+						yes++;
+					else
+						if(answer.getOption().trim().matches(Answer.NO) || answer.getOption().trim().matches(Answer.PROBABLY_NOT))
+							no++;
+						else
+							if(answer.getOption().matches(Answer.I_CANT_TELL))
+								iCantTell++;
+				}
+				total = yes+ no+iCantTell;
+				System.out.print("total|"+total);
+				System.out.print("| #Yes|"+yes+ "|"+ formatter.format(yes/(yes+no)*100)+ "%");
+				System.out.print("| #Nos|"+no + "|"+ formatter.format(no/(yes+no)*100)+ "%");
+				System.out.print("| #ICantTell|"+iCantTell + "|"+ formatter.format(iCantTell/(total)*100)+ "%");
+				System.out.println();
+			}		
+		}
+
+		
+	
+			
+		System.out.println("-------------------------------------------");
+	
+
+	}
+	
 	public void expectedNoAnswers(Point point,String fileName, HashMap<String, Microtask>microtaskMap, HashMap<String,String> yesMap){
 			double counterYes=0;
 			double counterAnswers=0;
@@ -144,13 +198,13 @@ public class LogAnalysis {
 			//System.out.println(" *** NO-BUG REVEALING QUESTIONS - EXPECTED No/Prob No ANSWERS: ");
 			counterAnswers = counterYes+counterNos+counterICantTell;
 			System.out.println(fileName+" NO-BUG Expected Answers="+counterAnswers);
-			System.out.print("#Yes="+counterYes+ " = "+ formatter.format(counterYes/(counterNos+counterYes)*100)+ "%, ");
-			System.out.print("#Nos="+counterNos + " = "+ formatter.format(counterNos/(counterYes+counterNos)*100)+ "%, ");
-			System.out.println("#ICantTell="+counterICantTell + " = "+ formatter.format(counterICantTell/(counterAnswers)*100)+ "%");
+			System.out.println("counterYes="+counterYes+ " = "+ formatter.format(counterYes/(counterNos+counterYes)*100)+ "%");
+			System.out.println("counterNos="+counterNos + " = "+ formatter.format(counterNos/(counterYes+counterNos)*100)+ "%");
+			System.out.println("counterICantTell="+counterICantTell + " = "+ formatter.format(counterICantTell/(counterAnswers)*100)+ "%");
 			
 			this.counterMap.put(fileName,new Counter(counterYes,counterNos, counterICantTell, fileName,counterAnswers));
 			
-			//System.out.println("-------------------------------------------");
+			System.out.println("-------------------------------------------");
 			
 		
 
@@ -172,16 +226,16 @@ public class LogAnalysis {
 		public String fileName;
 		public double totalAnswers;
 	}		
+
+
 	
-	public static void main(String[] args){
-		//String path = "C:\\Users\\adrianoc\\Dropbox (PE-C)\\3.Research\\1.Fall2014-Experiments\\Production-2October25\\1Final\\";
-		String path = "C:\\Users\\Christian Adriano\\Dropbox (PE-C)\\3.Research\\1.Fall2014-Experiments\\Production-2October25\\1Final\\";
-		
-		LogData data = new LogData(true, new Double(240000).doubleValue());
+	private static LogAnalysis initializeLogs(){
+		String path = "C:\\Users\\adrianoc\\Dropbox (PE-C)\\3.Research\\1.Fall2014-Experiments\\Production-2October25\\1Final\\";
+		//String path = "C:\\Users\\Christian Adriano\\Dropbox (PE-C)\\3.Research\\1.Fall2014-Experiments\\Production-2October25\\Snapshot12\\";
+		LogData data = new LogData(true, 0);
 		data.processLogProduction2(path);
 		
-		//path = "C:\\Users\\adrianoc\\Dropbox (PE-C)\\3.Research\\1.Fall2014-Experiments\\Production-1\\Oct25\\SnapShot12\\";
-		path = "C:\\Users\\Christian Adriano\\Dropbox (PE-C)\\3.Research\\1.Fall2014-Experiments\\Production-1\\Oct25\\SnapShot12\\";
+		path = "C:\\Users\\adrianoc\\Dropbox (PE-C)\\3.Research\\1.Fall2014-Experiments\\Production-1\\Oct25\\SnapShot12\\";
 		data.processLogProduction1(path);
 		
 		LogAnalysis analysis = new LogAnalysis(data);
@@ -195,19 +249,27 @@ public class LogAnalysis {
 		System.out.println("Sessions Closed: "+data.getClosedSessions());
 		System.out.println("Answers: "+data.getNumberOfMicrotasks());
 		
-		System.out.println("Answer Durations (minutes): "); 
-		System.out.println("  below 1:"+ data.ZeroToOne);
-		System.out.println("  1 to 2:"+ data.OneToTwo);
-		System.out.println("  2 to 3:"+ data.TwoToThree);
-		System.out.println("  3 to 4:"+data.ThreeToFour);
-		System.out.println("  4 to 5:"+data.FourToFive);
-		System.out.println("  5 to 6:"+data.FiveToSix);
-		System.out.println("  above 6:"+data.aboveSix);
-		//Configure the MicrotaskMap
+		return analysis;
+	}
+	
+	public static void main(String[] args){
 		
+		LogAnalysis analysis = initializeLogs();
+		LogData data = analysis.data;
+		System.out.println("EXPECTED YES ANSWERS");
+		analysis.answersPerMethod(true, data.yesMap, data.microtaskMap);
+		
+		//System.out.println("EXPECTED NO ANSWERS");
+		//analysis.answersPerMethod(false, data.yesMap, data.microtaskMap);
+
+	
+	}
+	
+	
+	private void expectedYesNos_Unfiltered(){
 		//General
-		analysis.expectedYesAnswers(new Point(0,214),"Total", data.microtaskMap, data.yesMap);
-		analysis.expectedNoAnswers(new Point(0,214),"Total", data.microtaskMap, data.yesMap);
+		this.expectedYesAnswers(new Point(0,214),"Total", data.microtaskMap, data.yesMap);
+		this.expectedNoAnswers(new Point(0,214),"Total", data.microtaskMap, data.yesMap);
 		//analysis.expectedYesAnswersLessMethodBody();
 		
 		
@@ -221,9 +283,9 @@ public class LogAnalysis {
 		iter = data.fileNameTaskRange.keySet().iterator();
 		while(iter.hasNext()){
 			String fileName = iter.next();
-			//System.out.print("BUG REPORT: "+ fileName+", ");
+			System.out.print("BUG REPORT: "+ fileName+", ");
 			Point point = data.fileNameTaskRange.get(fileName);
-			analysis.expectedYesAnswers(point,fileName,data.microtaskMap, data.yesMap);
+			this.expectedYesAnswers(point,fileName,data.microtaskMap, data.yesMap);
 		}
 		
 		System.out.println();
@@ -232,38 +294,39 @@ public class LogAnalysis {
 		iter = data.fileNameTaskRange.keySet().iterator();
 		while(iter.hasNext()){
 			String fileName = iter.next();
-			//System.out.print("BUG REPORT: "+ fileName+", ");
+			System.out.print("BUG REPORT: "+ fileName+", ");
 			Point point = data.fileNameTaskRange.get(fileName);
-			analysis.expectedNoAnswers(point,fileName,data.microtaskMap, data.yesMap);
+			this.expectedNoAnswers(point,fileName,data.microtaskMap, data.yesMap);
 		}
 	
-		System.out.println();
-	//
-	//Configure the MicrotaskMap
+	}
+	
+	
+	private void expectedFilterCompleteSessions(){
 		
-	data.computeMicrotaskFromCompleteSessions();
+		data.computeMicrotaskFromCompleteSessions();
 
-	data.workingMicrotaskMap = data.completeSessions_microtaskMap;
-	/*
-	System.out.println("---- EXPECTED YES >>FILTER: ONLY COMPLETE SESSIONS ----");
-	iter = data.fileNameTaskRange.keySet().iterator();
-	while(iter.hasNext()){
-		String fileName = iter.next();
-		Point point = data.fileNameTaskRange.get(fileName);
-		analysis.expectedYesAnswers(point,fileName,data.microtaskMap, data.yesMap);
-	}
-	
-	System.out.println();
-	
-	System.out.println("---- EXPECTED NO >>FILTER: ONLY COMPLETE SESSIONS ----");
-	iter = data.fileNameTaskRange.keySet().iterator();
-	while(iter.hasNext()){
-		String fileName = iter.next();
-		Point point = data.fileNameTaskRange.get(fileName);
-		analysis.expectedNoAnswers(point,fileName,data.microtaskMap, data.yesMap);
-	}
-	*/
-	
+		data.workingMicrotaskMap = data.completeSessions_microtaskMap;
+		
+		System.out.println("---- EXPECTED YES >>FILTER: ONLY COMPLETE SESSIONS ----");
+		Iterator<String> iter = data.fileNameTaskRange.keySet().iterator();
+		while(iter.hasNext()){
+			String fileName = iter.next();
+			System.out.print("BUG REPORT: "+ fileName+", ");
+			Point point = data.fileNameTaskRange.get(fileName);
+			this.expectedYesAnswers(point,fileName,data.microtaskMap, data.yesMap);
+		}
+		
+		System.out.println();
+		
+		System.out.println("---- EXPECTED NO >>FILTER: ONLY COMPLETE SESSIONS ----");
+		iter = data.fileNameTaskRange.keySet().iterator();
+		while(iter.hasNext()){
+			String fileName = iter.next();
+			System.out.print("BUG REPORT: "+ fileName+", ");
+			Point point = data.fileNameTaskRange.get(fileName);
+			this.expectedNoAnswers(point,fileName,data.microtaskMap, data.yesMap);
+		}
 	}
 	
 }
