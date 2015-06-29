1a86f26d16387834ed2cd3a29a9a7a9a00bc65b8
Nathan Barreto <nathanmrtns@gmail.com>
2015-06-29 13:04:25 -0700

    QUestions added in the file

--- samples/bulkSkillTests/PrintDatastructure.java
+++ samples/bulkSkillTests/PrintDatastructure.java
@@ -1,13 +1,10 @@
-package pretask.test;
-import java.util.*;
-
 public class PrintDatastructure {
 
 	public static void main(String[] args){
 		
 		HashMap<String, String[]> rootMap =  new HashMap<String, String[]>();
 		String[] list1 = { "A","B"} ;
-		String[] list2 = {};
+		String[] list2 = {{}};
 		
 		rootMap.put("0", list1);
 		rootMap.put("1", null);
@@ -33,3 +30,9 @@ public class PrintDatastructure {
 		System.out.println(";");		
 	}
 }
+
+//What is the output of the code above ?
+//What is the output if in the line 10 was rootMap.put(null,null)?
+//What is the output if in the line 11 was rootMap.put(list1,list2)?
+//What is the output if in the line 27 was for(int i=0; i<list.length; i++)?
+//What is the output if list2 = {"B", "A"}, line 9 was rootMap.put("",list1), erase line 10 and line 11 was rootMap.put("",list2)?
