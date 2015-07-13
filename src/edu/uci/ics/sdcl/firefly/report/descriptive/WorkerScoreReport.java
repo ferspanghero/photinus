e1e18eedc11b4e2501a0a29f213f60f137c88b94
nathanmrtns <nathanmrtns@gmail.com>
2015-07-13 16:59:08 -0700

    Worker Score report first 2 tables

---
+++ src/edu/uci/ics/sdcl/firefly/report/descriptive/WorkerScoreReport.java
@@ -0,0 +1,31 @@
+package edu.uci.ics.sdcl.firefly.report.descriptive;
+
+import java.util.Iterator;
+
+import edu.uci.ics.sdcl.firefly.Answer;
+import edu.uci.ics.sdcl.firefly.Worker;
+
+public class WorkerScoreReport extends AnswerReport{
+	
+	String consentLogpath = "C:/var/lib/tomcat7/webapps/consent-Pilot2-log.txt";
+	
+	@Override
+	public String getType() {
+		return "Workers Scores for each answer";
+	}
+
+	@Override
+	protected String reportData(Answer answer) {
+		FileConsentDTO fc = new FileConsentDTO(consentLogpath);
+
+		Iterator<Worker> workersIter = fc.getWorkers().values().iterator();
+		while(workersIter.hasNext()){
+			Worker worker = workersIter.next();
+			if(worker.getWorkerId().compareTo(answer.getWorkerId())==0){
+				return worker.getGrade().toString();
+			}
+		}
+		return null;
+	}
+
+}
