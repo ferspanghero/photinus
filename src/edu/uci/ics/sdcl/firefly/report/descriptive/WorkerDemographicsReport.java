3ffcced6fb8c992d238102ef749d36681c73dc35
nathanmrtns <nathanmrtns@gmail.com>
2015-07-13 15:17:44 -0700

    WorkerDemographicsReport done

--- src/edu/uci/ics/sdcl/firefly/report/descriptive/WorkerDemographicsReport.java
+++ src/edu/uci/ics/sdcl/firefly/report/descriptive/WorkerDemographicsReport.java
@@ -2,38 +2,26 @@ package edu.uci.ics.sdcl.firefly.report.descriptive;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.Map.Entry;
 
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.apache.poi.ss.util.CellRangeAddress;
 
 import edu.uci.ics.sdcl.firefly.Answer;
 import edu.uci.ics.sdcl.firefly.Microtask;
 import edu.uci.ics.sdcl.firefly.Worker;
 import edu.uci.ics.sdcl.firefly.WorkerSession;
-import edu.uci.ics.sdcl.firefly.servlet.SurveyServlet;
-import edu.uci.ics.sdcl.firefly.util.PropertyManager;
 
 public class WorkerDemographicsReport {
 
 private String fileName = "WorkersDemographicsReport.xlsx";
 	
 	String consentLogpath = "C:/var/lib/tomcat7/webapps/consent-Pilot2-log.txt";
-	String sessionLogpath = "C:/var/lib/tomcat7/webapps/session-Pilot2-log.log";
+	String sessionLogpath = "C:/var/lib/tomcat7/webapps/session-log.log";
 
 	public WorkerDemographicsReport(){
 		writeToXlsx();
@@ -46,7 +34,6 @@ private String fileName = "WorkersDemographicsReport.xlsx";
 
 		//Create a blank sheet (for the scores)
 		XSSFSheet workersSheet = workbook.createSheet("Workers");
-		
 		//first header
 		createFirstHeader(workersSheet);
 		//second header
@@ -197,6 +184,7 @@ private String fileName = "WorkersDemographicsReport.xlsx";
 	}
 
 	public void populateDataIdentifiers(Row row, int cellNum, Worker worker) {
+
 		Cell cell;
 		cell = row.createCell(cellNum++);
 		cell.setCellValue(worker.getWorkerId());
@@ -254,6 +242,7 @@ private String fileName = "WorkersDemographicsReport.xlsx";
 				}else{
 					numberOfNo++;
 				}
+				//72;73;78;79;84;92;95;97;102;104;119;123;126			}
 			}
 		}
 		int numberOfTasks = workerSessions.size();
@@ -282,6 +271,113 @@ private String fileName = "WorkersDemographicsReport.xlsx";
 		
 		cell = row.createCell(cellNum++);
 		cell.setCellValue(durationAverage);
+		
+		populateOptimismAnalysis(row,cellNum, worker, numberOfYes, numberOfNo);
+	}
+	
+	public void populateOptimismAnalysis(Row row, int cellNum, Worker worker, int numberOfYes, int numberOfNo){
+		FileSessionDTO fs = new FileSessionDTO(sessionLogpath);
+		ArrayList<WorkerSession> workerSessions = fs.getSessionsByWorkerID(worker.getWorkerId());
+		int totalAnswers= numberOfYes+numberOfNo;
+		int expectedYes = 0;
+		int expectedNo = 0;
+		for (WorkerSession workerSession : workerSessions) {
+			Iterator<Microtask> iterMicrotask = workerSession.getMicrotaskList().iterator();
+			while(iterMicrotask.hasNext()){
+				Microtask microtask = iterMicrotask.next();
+				Answer answer = microtask.getAnswerByUserId(worker.getWorkerId());
+				if(isBugCovering(microtask.getID())){
+					expectedYes++; 
+				}
+			}
+		}
+		expectedNo = totalAnswers - expectedYes;
+		
+		Cell cell;
+		cell = row.createCell(cellNum++);
+		cell.setCellValue(expectedYes);
+		
+		cell = row.createCell(cellNum++);
+		cell.setCellValue(expectedNo);
+		
+		if(expectedYes!=0){
+			cell = row.createCell(cellNum++);
+			cell.setCellValue(numberOfYes/Double.parseDouble(expectedYes+""));
+		}else{
+			cell = row.createCell(cellNum++);
+			cell.setCellValue(0);
+		}
+		
+		if(expectedNo != 0){
+			cell = row.createCell(cellNum++);
+			cell.setCellValue(numberOfNo/Double.parseDouble(expectedNo+""));
+		}else{
+			cell = row.createCell(cellNum++);
+			cell.setCellValue(0);
+		}
+		
+		populateQuality(row, cellNum, worker);
+	}
+	
+	private void populateQuality(Row row, int cellNum, Worker worker) {
+		FileSessionDTO fs = new FileSessionDTO(sessionLogpath);
+		ArrayList<WorkerSession> workerSessions = fs.getSessionsByWorkerID(worker.getWorkerId());
+		int truePositive = 0;
+		int trueNegative = 0;
+		int falsePositive = 0;
+		int falseNegative = 0;
+		int idk=0; 
+		for (WorkerSession workerSession : workerSessions) {
+			Iterator<Microtask> iterMicrotask = workerSession.getMicrotaskList().iterator();
+			while (iterMicrotask.hasNext()) {
+				Microtask microtask = iterMicrotask.next();
+				Answer answer = microtask.getAnswerByUserId(worker.getWorkerId());
+				String answerOption = answer.getOption();
+				boolean bugCovering = isBugCovering(microtask.getID());
+				if(answer!=null){
+					if (bugCovering && answerOption.equals(Answer.YES)) {
+						truePositive++;
+					} else if (!bugCovering && answerOption.equals(Answer.NO)) {
+						trueNegative++;
+					} else if (!bugCovering && answerOption.equals(Answer.YES)) {
+						falsePositive++;
+					} else if (bugCovering && answerOption.equals(Answer.NO)) {
+						falseNegative++;
+					} else {
+						idk++;
+					}
+				}
+			}
+		}
+		int total = truePositive+trueNegative+falseNegative+falsePositive+idk;
+		Cell cell;
+		cell = row.createCell(cellNum++);
+		cell.setCellValue(truePositive);
+		
+		cell = row.createCell(cellNum++);
+		cell.setCellValue(trueNegative);
+		
+		cell = row.createCell(cellNum++);
+		cell.setCellValue(falsePositive);
+		
+		cell = row.createCell(cellNum++);
+		cell.setCellValue(falseNegative);
+		
+		cell = row.createCell(cellNum++);
+		cell.setCellValue(idk);
+		
+		cell = row.createCell(cellNum++);
+		cell.setCellValue(total);
+	}
+
+	public boolean isBugCovering(int microtaskID){
+		Integer[] bugCoveringQuestions = {72,73,78,79,84,92,95,97,102,104,119,123,126};
+		for(int i=0; i<bugCoveringQuestions.length; i++){
+			if(microtaskID == bugCoveringQuestions[i]){
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	public int getDifficultyAverage(ArrayList<WorkerSession> workerSessions, Worker worker) {
