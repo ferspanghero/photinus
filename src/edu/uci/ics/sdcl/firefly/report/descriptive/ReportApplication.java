a7ab4f8bed1620a74916ffb8b6ca0068b50d86aa
igMoreira <iago.silva@al.infnet.edu.br>
2015-07-23 14:58:51 -0700

    Built-in reports on ReportApplication

--- src/edu/uci/ics/sdcl/firefly/report/descriptive/ReportApplication.java
+++ src/edu/uci/ics/sdcl/firefly/report/descriptive/ReportApplication.java
@@ -6,7 +6,7 @@ public class ReportApplication {
 	private static DescriptiveReportBuilder builder;
 	
 	public static void main(String[] args) {
-		difficultyLevelReport(new ExcelExporter());
+		workerScoreReport(new ExcelExporter());
 		DescriptiveReport report = builder.generateDescriptiveReport();
 		report.exportReport(); // THIS SHOULD EXPORT ON AN EXCEL SPREADSHEET
 	}
@@ -29,7 +29,39 @@ public class ReportApplication {
 	private static void answerDurationReport(DescriptiveReportWriter exporter)
 	{
 		//TODO: the count report and the correctness report are different
+		throw new UnsupportedOperationException("The answerDuration report is not implemented yet");
 //		builder = new DescriptiveReportBuilder(new AnswerDuration(), new , correctness, exporter)
+	}
+	
+	private static void sizeOfExplanationReport(DescriptiveReportWriter exporter)
+	{
+		//TODO: the count report and the correctness report are different
+		throw new UnsupportedOperationException("The sizeOfExplanation report is not implemented yet");
+//		builder = new DescriptiveReportBuilder(new AnswerExplanationSize(), counter, correctness, exporter);
+	}
+	
+	private static void durationOfAnswersAgroupedByOrder(DescriptiveReportWriter exporter)
+	{
+		//TODO: Verify the count report and the correctness details
+		throw new UnsupportedOperationException("The durationByOrder report is not implemented yet");
+//		builder = new DescriptiveReportBuilder(new OrderDuration(), counter, correctness, exporter);
+	}
+	
+	private static void sessionDurationReport(DescriptiveReportWriter exporter)
+	{
+		//TODO: Verify the count report and the correctness details
+		throw new UnsupportedOperationException("The sessionDuration report is not implemented yet");
+//		builder = new DescriptiveReportBuilder(new SessionDuration(), counter, correctness, exporter);
+	}
+	
+	private static void workerScoreReport(DescriptiveReportWriter exporter)
+	{
+		builder = new DescriptiveReportBuilder(new WorkerScoreReport(), new AverageWorkerProfileReport(), new AverageCorrectness(), exporter);
+	}
+	
+	private static void workerDemographicsReport()
+	{
+		new WorkerDemographicsReport();
 	}
 	
 }
