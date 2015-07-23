15500478d3e86bd3c39d0cd82801dcb9186b679e
igMoreira <iago.silva@al.infnet.edu.br>
2015-07-23 13:32:29 -0700

    ConfidenceCounting - CountReport

---
+++ src/edu/uci/ics/sdcl/firefly/report/descriptive/ConfidenceCounting.java
@@ -0,0 +1,70 @@
+package edu.uci.ics.sdcl.firefly.report.descriptive;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import edu.uci.ics.sdcl.firefly.Answer;
+import edu.uci.ics.sdcl.firefly.Microtask;
+
+/**
+ * 
+ * @author igMoreira
+ * 
+ * Counting part of the report, offers the
+ * number of answers grouped by confidence
+ * level of a given question.
+ *
+ */
+public class ConfidenceCounting extends CountReport {
+
+	/**
+	 * Generates the CountReport content part. 
+	 */
+	@Override
+	public Map<String, List<String>> generateReport( Map<String, List<String>> content) {
+		List<String> questionIDList = content.get("Question ID");
+		List<List<String>> confidenceList = new ArrayList<List<String>>();
+		for (int i = 0; i < 6; i++) {
+			confidenceList.add(new ArrayList<String>());
+		}
+		if(questionIDList != null)
+		{
+			SessionDTO dto = new FileSessionDTO();
+			Map<String, Microtask> microtasks = dto.getMicrotasks();
+			int[] confidence = null;
+			for (String questionID : questionIDList) {
+				confidence = new int[6];
+				Arrays.fill(confidence, 0);
+				Microtask question = microtasks.get(questionID);
+				for (Answer answer : question.getAnswerList()) {
+					confidence[answer.getConfidenceOption()]++;
+				}
+				for (int i = 0; i < confidence.length; i++) {
+					confidenceList.get(i).add(Integer.toString(confidence[i]));
+				}
+			}
+			for (int i = 0; i < confidenceList.size(); i++) {
+				this.countContent.put("# Confidence level - "+i, confidenceList.get(i));
+			}
+		}
+		else
+		{
+			System.out.println("Invalid question ID came from the HeaderReport");
+			System.exit(0);
+		}
+		return this.countContent;
+	}
+
+	/**
+	 * Returns the type of the report,
+	 * this will be used on the ReportWriter
+	 * to build the titles.
+	 */
+	@Override
+	public String getType() {
+		return "Derived Data - Counting";
+	}
+
+}
