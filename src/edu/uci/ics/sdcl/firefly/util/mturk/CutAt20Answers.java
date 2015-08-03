185532d9377eef1e65bcdc383d5cc680d371936b
Christian Medeiros Adriano <adrianoc@uci.edu>
2015-08-02 20:43:49 -0700

    Replaced % symbol in explanations for the word "percent"

--- src/edu/uci/ics/sdcl/firefly/util/mturk/CutAt20Answers.java
+++ src/edu/uci/ics/sdcl/firefly/util/mturk/CutAt20Answers.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 
 /**
  * Remove entries from SessionLog that exceed 20 answers per question
+ * Also replaces the % symbol for the word "percent" in the workers explanations
  * 
  * @author adrianoc
  *
@@ -24,11 +25,14 @@ public class CutAt20Answers {
 		LogReadWriter logReadWriter = new LogReadWriter();
 		ArrayList<String> buffer = logReadWriter.readToBuffer(4,sessionLogFileName);
 
-		ArrayList<String> newBuffer = this.removeExcessiveAnswers(buffer);
-		logReadWriter.writeBackToBuffer(newBuffer, 4, sessionLogFileName_Cut);
-		
-		this.printMicrotaskWithMoreThan20Answers();
-		//this.checkAllMicrotasksHave20Answers(newBuffer);
+		buffer = this.removeExcessiveAnswers(buffer);
+
+		buffer = this.replaceExplanationsWithPercentage(buffer);
+
+		logReadWriter.writeBackToBuffer(buffer, 4, sessionLogFileName_Cut);
+
+		//this.printMicrotaskWithMoreThan20Answers();
+		this.checkAllMicrotasksHave20Answers(buffer);
 	}
 
 
@@ -47,7 +51,7 @@ public class CutAt20Answers {
 					newBuffer.set(i,line);
 				}
 				else{
-					newBuffer.set(i, "/n");
+					newBuffer.set(i,"\n");
 				}
 			}
 		}
@@ -80,10 +84,10 @@ public class CutAt20Answers {
 			} 
 		}
 	}
-	
-	
+
+
 	private void printMicrotaskWithMoreThan20Answers(){
-	
+
 		int i=0;
 		for(String microtaskID: this.microtaskAnswerCountMap.keySet()){
 			Integer count = this.microtaskAnswerCountMap.get(microtaskID);
@@ -97,9 +101,9 @@ public class CutAt20Answers {
 
 
 	private void checkAllMicrotasksHave20Answers(ArrayList<String>buffer){
-	
+
 		HashMap<String,Integer> microtaskCounter = new HashMap<String,Integer>();
-		
+
 		for(int i=0; i<buffer.size(); i++){
 			String line = buffer.get(i);
 			if(line.contains("MICROTASK")){
@@ -111,7 +115,7 @@ public class CutAt20Answers {
 				microtaskCounter.put(id,counter);
 			}
 		}
-			
+
 		int i=0;
 		for(String microtaskID: microtaskCounter.keySet()){
 			Integer count = microtaskCounter.get(microtaskID);
@@ -122,11 +126,89 @@ public class CutAt20Answers {
 		}
 		System.out.println("Total problematic microtasks: "+i);
 	}
-	
-	
+
+
+	private ArrayList<String> replaceExplanationsWithPercentage(ArrayList<String> buffer){
+
+		String[] sessionList = {
+				"38ae9a-2G-80-4",
+				"221gI-9E7a1-3-9",
+				"163eE-5c6C-8-21",
+				"352eG6G8I245",
+				"760gA-5G-9C-2-76",
+				"315EA-8G8E705",
+				"417AC-1e-3I5-50",
+				"72iG-9A-8i02-1",
+				"29CA-5C6g71-2",
+			"5gI3C-9i-50-7"};
+
+		String[] microtaskList = {
+				"14",
+				"25",
+				"18",
+				"86",
+				"97",
+				"81",
+				"1",
+				"52",
+				"81",
+		"47"};
+
+		HashMap<String, String> sessionMicrotaskMap = new HashMap<String,String>();
+
+		for(int i=0; i<sessionList.length;i++){
+			sessionMicrotaskMap.put(sessionList[i], microtaskList[i]);
+		}
+
+		ArrayList<String> newBuffer = (ArrayList<String>) buffer.clone();
+
+		for(int i=0; i<buffer.size(); i++){
+			String line = buffer.get(i);
+			if(line.contains("MICROTASK")){
+				String sessionID = line.split("%")[7];
+				if(sessionMicrotaskMap.containsKey(sessionID)){
+					String sessionMicrotaskID = sessionMicrotaskMap.get(sessionID);
+					String logMicrotaskID = line.split("%")[9];
+					if(sessionMicrotaskID.compareTo(logMicrotaskID)==0){
+						newBuffer.set(i, this.replacePercentSymbol(line));
+					}
+				}
+			}
+		}	
+		return newBuffer;
+	}
+
+
+	private String replacePercentSymbol(String line){
+
+		String[] elements = line.split("%");
+		if(elements.length<25){//Means that there is no % symbol in the explanation text.
+			//System.out.println(line);
+			//System.out.println(line.split("\t")[0]);
+			//System.out.println(line.split("\t")[1]);
+			return line;
+		}
+		else{
+			String explanationPart1 = line.split("%")[23];
+			String explanationPart2 = line.split("%")[24];
+			int end = line.lastIndexOf("explanation") + "explanation%".length();
+			String subLine = line.substring(0, end);
+			subLine = subLine + explanationPart1+ " percent" + explanationPart2;
+			return subLine;
+		}
+	}
+
+	public void testReplacePercentSymbol(){
+		String input = "http-bio-8080-exec-8925] INFO  - EVENT%MICROTASK% workerId%27Ei-8i0A-3-59:15cG-7i7C996% fileName%HIT02_24% sessionId%38ae9a-2G-80-4% microtaskId%14% questionType%VARIABLE_DECLARATION% question%Is there any issue with the use or the definition of variable 'g' in the source code below that might be related to the failure?% answer%YES, THERE IS AN ISSUE% confidenceLevel%4% difficulty%3% duration%247533.0% explanation%The calculation of g appears to result in a negative integer value if the parameter value is negative to begin with. Passing a negative g into Color would produce the error and there is nothing in the definition of g that seeks to prevent that. And the calculation certainly looks like it would come out negative in that scenario. Again without understanding the relevance of lowerbound and upperbound, I cannot be 100% certain, as a caveat.";		
+		System.out.println(this.replacePercentSymbol(input));
+	}
+
+	//----------------------------------------------------------------------------------------------
 	public static void main(String[] args){
 		CutAt20Answers cutter = new CutAt20Answers();
 		cutter.run();
+
+		//cutter.testReplacePercentSymbol();
 	}
 
 }
